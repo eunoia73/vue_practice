db768098d61e2074eeef786c7e1e2ad768e68d64
eunoia73 <soo97703@naver.com>
2026-04-08 12:46:46 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ShoppingCart from './components/ShoppingCart.vue'
+</script>
+
+<template>
+  <div>
+    <!-- 쇼핑카트 예제(computed) -->
+    <ShoppingCart />
+  </div>
+</template>
