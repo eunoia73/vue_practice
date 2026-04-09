e7be578b43e20fa5010028c550c236f06747f2e9
eunoia73 <soo97703@naver.com>
2026-04-09 14:27:43 +0900

    feat: #7 Teleport 모달 예제

---
+++ src/components/ModalTeleport.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="showModal = true">모달 열기</button>
+    <Teleport to="body">
+      <div v-if="showModal" class="modal">
+        <div class="modal-content">
+          <h2>모달 제목</h2>
+          <p>모달 내용</p>
+          <button @click="showModal = false">닫기</button>
+        </div>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const showModal = ref(false)
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modal-content {
+  background: white;
+  padding: 2rem;
+  border-radius: 8px;
+}
+</style>
